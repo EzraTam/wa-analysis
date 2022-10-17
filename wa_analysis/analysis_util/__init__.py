aa03749cce55936060d508896382b9ab76aa641b
Ezra Tampubolon <ezra.tampubolon@googlemail.com>
2022-10-17 22:27:58 +0200

    Create computation of sentiment

--- wa_analysis/analysis_util/__init__.py
+++ wa_analysis/analysis_util/__init__.py
@@ -4,3 +4,5 @@
 from . import analysis_util
 from . import preprocessing
 from . import processing_meta
+from . import sentiment_from_dict
+
---
+++ wa_analysis/analysis_util/sentiment_from_dict.py
@@ -0,0 +1,57 @@
+"""Module for computation of sentiment scores based on dictionaries
+Data inputs are given in the bag of words form.
+"""
+import os
+import json
+
+path_data=path_data=os.path.join(os.path.dirname(__file__),"data")
+
+class ComputeSentimentFromDict:
+    """Compute the sentiment of a feature vector
+    by means of a dict
+    """
+
+    def __init__(self,feature_vector:list[str]):
+        
+        self.feature_vector=feature_vector
+
+        self.li_sentiment=["sentiment_positive_scores","sentiment_negative_scores"]
+
+        # Load dict
+        self.dict_sentiment={}
+        for nm_sent in self.li_sentiment:
+            with open('data.json') as f:
+                self.dict_sentiment[nm_sent] = json.load(f)
+
+        self.score=0
+        self.polarity=""
+
+    def compute_sentiment_score(self):
+        """Compute sentiment score by aggregating
+        over positive and negative sentiment
+        """
+        self.score = 0
+
+        for nm_sent in self.li_sentiment:
+            for word in self.feature_vector:
+                try:
+                    self.score=self.score+ self.dict_sentiment[nm_sent][word]
+                except KeyError:
+                    pass
+
+    def compute_polarity(self):
+        """Compute polarity by thresholding
+        the sentiment score
+        """
+        if (self.score > 0):
+            self.polarity = 'positive'
+        elif (self.score < 0):
+            self.polarity = 'negative'
+        else:
+            self.polarity = 'neutral'
+        return dict(
+            score=self.score,
+            polarity=self.polarity
+        )
+
+
