a7def410d8f2f28ef1484643c89a6d5b814f2ac7
Ezra Tampubolon <ezra.tampubolon@googlemail.com>
2022-10-18 22:14:41 +0200

    add emoji

--- wa_analysis/analysis_util/processing_meta.py
+++ wa_analysis/analysis_util/processing_meta.py
@@ -1,6 +1,8 @@
 """Function configurations for cleaning texts
 """
 
+import re
+
 processing_by_re_meta = dict(
     url=dict(
         pattern=r"((www\.[^\s]+)|(https?://[^\s]+))",
@@ -11,4 +13,15 @@ processing_by_re_meta = dict(
     at_user=dict(pattern=r"@[A-Za-z0-9]+", replacement_str="at_user", flags=0, capture=True),
     additional_white_space=dict(pattern=r"[\s]+", replacement_str=" ", flags=0, capture=False),
     hash_tag=dict(pattern=r"#([^\s]+)", replacement_str=r"\1", flags=0, capture=True),
+    emoji= dict(
+        pattern="["
+        "\U0001F600-\U0001F64F"  # emoticons
+        "\U0001F300-\U0001F5FF"  # symbols & pictographs
+        "\U0001F680-\U0001F6FF"  # transport & map symbols
+        "\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        "]+",
+        replacement_str=" ",
+        flags=re.UNICODE,
+        capture=True
+    )
 )
